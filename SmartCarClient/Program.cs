2cad97383d45580b2a4386feae4e645c23a7b313
Alessandro Bianchin <alessandrobianchin311@gmail.com>
2019-01-10 22:08:19 +0100

    MQTT branch

--- SmartCarClient/Program.cs
+++ SmartCarClient/Program.cs
@@ -12,22 +12,50 @@ namespace SmartCarClient
             MessageQueuing queue = new MessageQueuing();
             ThreadStart threadDelegate = new ThreadStart(MQTTSender.sendData);
             Thread t = new Thread(threadDelegate);
-            RandomGenerator random = new RandomGenerator(); 
-            TemperatureSensorInterface temp = new VirtualTemperatureSensor(random);
-            SpeedSensorInterface speed = new VirtualSpeedSensor(random);
-            GPSSensorInterface gps = new VirtualGPSSensor(random);
+            RandomGenerator random;
+            TemperatureSensorInterface temp;
+            SpeedSensorInterface speed;
+            GPSSensorInterface gps;
             //DirectionSensorInterface dir = new VirtualDirectionSensor();
 
             t.Start();
 
             while (true)
             {
+                random = new RandomGenerator();
+                temp = new VirtualTemperatureSensor(random);
+                Thread.Sleep(1);
+                speed = new VirtualSpeedSensor(random);
+                Thread.Sleep(1);
+                gps = new VirtualGPSSensor(random);
                 long epoch = (long)(DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalMilliseconds;
-                
-                queue.addMessage("{" + string.Format(" \"value\": {0} , \"timestamp\": {1} ", temp.getTemperature(), epoch) + "}","temperature");
-                queue.addMessage("{" + string.Format(" \"value\": {0} , \"timestamp\": {1} ", speed.getSpeed(), epoch) + "}", "speed");
-                queue.addMessage("{" + string.Format(" \"value\": {0} , \"timestamp\": {1} ", gps.getLat(), epoch) + "}", "GPS/lat");
-                queue.addMessage("{" + string.Format(" \"value\": {0} , \"timestamp\": {1} ", gps.getLon(), epoch) + "}", "GPS/lon");
+                var tempJson = new
+                {
+                    value = temp.getTemperature(),
+                    timestamp = epoch
+                };
+
+                var speedJson = new
+                {
+                    value = speed.getSpeed(),
+                    timestamp = epoch
+                };
+                var latjson = new
+                {
+                    value = gps.getLat(),
+                    timestamp = epoch
+                };
+                var lonJson = new
+                {
+                    value = gps.getLon(),
+                    timestamp = epoch
+                };
+
+                queue.addMessage(JsonConvert.SerializeObject(tempJson),"temperature1");
+                queue.addMessage(JsonConvert.SerializeObject(speedJson), "speed1");
+                queue.addMessage(JsonConvert.SerializeObject(tempJson), "GPS/lat1");
+                queue.addMessage(JsonConvert.SerializeObject(tempJson), "GPS/lon1");
+
                 //queue.addMessage("{" + string.Format(" \"value\": {0} , \"timestamp\": {1} ", dir.getDirection(), epoch) + "}", "dir");
 
 
